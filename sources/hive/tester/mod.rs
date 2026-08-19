cbebccea4a86a8d1a6252ad83b30118deda895cb
kaishirojuancarlos <shirojuancarlos@gmail.com>
2026-08-19 08:50:03 +0200

    Implemented text concatenation macro.

--- sources/hive/tester/mod.rs
+++ sources/hive/tester/mod.rs
@@ -1,3 +1,8 @@
+use crate::concatenate;
+use crate::hive::text::SPACE;
+
 pub fn launch_test()
 {
+	let string = concatenate!("ALAINE!", " -- ", 2290, SPACE, '@');
+	println!(":: {string}");
 }
--- sources/hive/text/mod.rs
+++ sources/hive/text/mod.rs
@@ -2,3 +2,15 @@ pub const WHITESPACE: char = '\u{0}';
 pub const SPACE: char = ' ';
 pub const TAB: char = '\t';
 pub const NEWLINE: char = '\n';
+
+#[macro_export]
+macro_rules! concatenate {
+	($($value:expr),* $(,)?) => {{
+		use std::fmt::Write;
+		let mut output = String::new();
+		$(
+			write!(&mut output, "{}", $value).unwrap();
+		)*
+		output
+	}}
+}
